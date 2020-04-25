5fa13782b693a68486a42ad642533ab119ae7078
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2020-04-24 19:11:25 -0700

    Update datapreprocessing.py

--- src/datapreprocessing.py
+++ src/datapreprocessing.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import numpy.random as npr
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from scipy import sparse
 
-#Function to load gene expression and clinical data complete
 def load_GE(filename1, filename2):
+    '''
+    Function to load gene expression and clinical data complete
+    input: 
+        - filename1: gene expression data all
+        - filename2: clinical data all 
+    output:
+        - pre-processed data
+    '''
     df_ge = pd.read_csv(filename1,sep=';')
     df_cl = pd.read_csv(filename2,sep=';')
     #print(df_ge.shape,df_cl.shape)
@@ -59,8 +74,6 @@ def read_GE(filename1, filename2):
     train = np.matrix(train)
     return train,colnames, y01, [abr,gender]
 
-#filename = "data\\tcga_train_gexpression_cgc_7k.txt" #_2
-
 def data_prep(filename):
     '''
     parameters:
@@ -86,3 +99,76 @@ def data_prep(filename):
     j, v = train.shape
 
     return train, j, v, y01,  abr, colnames
+
+
+#Code bellow adapted from Yixin Wang
+def sim_genes_BN(Fs, ps, n_hapmapgenes, n_causes, n_units, D=3):
+    '''
+    inputs: 
+        - Fs: matrix (format?)
+        - ps: matrix (format?)
+        - n_hapmapgenes: possible causes
+        - n_causes: int/col
+        - n_units:  size/row
+    output:
+        - G: 
+        - lambdas: 
+    '''
+    
+    idx = npr.randint(n_hapmapgenes, size = n_causes)
+    p = ps[idx]
+    F = Fs[idx]
+    Gammamat = np.zeros((n_causes, D))
+    for i in range(D):
+        Gammamat[:,i] = npr.beta((1-F)*p/F, (1-F)*(1-p)/F)
+    S = npr.multinomial(1, (60/210, 60/210, 90/210), size = n_units)
+    F = S.dot(Gammamat.T)
+    G = npr.binomial(2, F)
+    lambdas = KMeans(n_clusters=3, random_state=123).fit(S).labels_
+    sG = sparse.csr_matrix(G)
+    return G, lambdas
+
+def sim_genes_TGP(Fs, ps, n_hapmapgenes, n_causes, n_units, hapmap_gene_clean, D=3):
+    pca = PCA(n_components=2, svd_solver='full')
+    S = expit(pca.fit_transform(hapmap_gene_clean))
+    Gammamat = np.zeros((n_causes, 3))
+    Gammamat[:,0] = 0.45*npr.uniform(size=n_causes)
+    Gammamat[:,1] = 0.45*npr.uniform(size=n_causes)
+    Gammamat[:,2] = 0.05*np.ones(n_causes)
+    S = np.column_stack((S[npr.choice(S.shape[0],size=n_units,replace=True),], \
+        np.ones(n_units)))
+    F = S.dot(Gammamat.T)
+    G = npr.binomial(2, F)
+    lambdas = KMeans(n_clusters=3, random_state=123).fit(S).labels_
+    sG = sparse.csr_matrix(G)
+    return G, lambdas
+
+def sim_genes_PSD(Fs, ps, n_hapmapgenes, n_causes, n_units, D=3):
+    alpha = 0.5
+    idx = npr.randint(n_hapmapgenes, size = n_causes)
+    p = ps[idx]
+    F = Fs[idx]
+    Gammamat = np.zeros((n_causes, D))
+    for i in range(D):
+        Gammamat[:,i] = npr.beta((1-F)*p/F, (1-F)*(1-p)/F)
+    S = npr.dirichlet((alpha, alpha, alpha), size = n_units)
+    F = S.dot(Gammamat.T)
+    G = npr.binomial(2, F)
+    lambdas = KMeans(n_clusters=3, random_state=123).fit(S).labels_
+    sG = sparse.csr_matrix(G)
+    return G, lambdas
+
+def sim_genes_SP(Fs, ps, n_hapmapgenes, n_causes, n_units, D=3):
+    a = 0.1
+    # simulate genes
+    Gammamat = np.zeros((n_causes, 3))
+    Gammamat[:,0] = 0.45*npr.uniform(size=n_causes)
+    Gammamat[:,1] = 0.45*npr.uniform(size=n_causes)
+    Gammamat[:,2] = 0.05*np.ones(n_causes)
+    S = npr.beta(a, a, size=(n_units, 2))
+    S = np.column_stack((S, np.ones(n_units)))
+    F = S.dot(Gammamat.T)
+    G = npr.binomial(2, F)
+    lambdas = KMeans(n_clusters=3, random_state=123).fit(S).labels_
+    sG = sparse.coo_matrix(G)
+    return G, lambdas
