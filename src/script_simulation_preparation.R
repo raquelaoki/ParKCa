3c4261693f948c68966246b7c63e80ea59910266
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2020-04-28 14:36:57 -0700

    pre-processing simulated data

---
+++ src/script_simulation_preparation.R
@@ -0,0 +1,15 @@
+#########################################
+# TGP data 
+#########################################
+
+rm(list=ls())
+setwd("C:/Users/raque/Documents/GitHub/ParKCa")
+
+#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#
+#Reference: https://github.com/knausb/vcfR
+
+if(!require(vcfR))install.packages('vcfR')
+
+require(vcfR)
+
+
