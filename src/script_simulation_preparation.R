34866cb493ed095fd6179ba1cc830d518eba9d90
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2020-04-29 17:40:35 -0700

    R code does not work
    memory problem

--- src/script_simulation_preparation.R
+++ src/script_simulation_preparation.R
@@ -12,4 +12,8 @@ if(!require(vcfR))install.packages('vcfR')
 
 require(vcfR)
 
+vcf <- read.vcfR( "data_s\\ALL.chip.omni_broad_sanger_combined.20140818.snps.genotypes.vcf.gz", verbose = FALSE)
+x <- vcfR2genlight(vcf)
+
+### memory problem 
 
