90120b0d1965924348c5df222c2da654631d3880
Raquel Aoki <23690490+raquelaoki@users.noreply.github.com>
2020-05-15 21:23:17 -0700

    NN

--- src/train.py
+++ src/train.py
@@ -34,7 +34,7 @@ import functools
 
 #Meta-leaners packages
 #https://github.com/aldro61/pu-learning
-from extra.puLearning.puAdapter import PUAdapter
+from puLearning.puAdapter import PUAdapter
 #https://github.com/t-sakai-kure/pywsl
 from pywsl.pul import pumil_mr, pu_mr
 from pywsl.utils.syndata import gen_twonorm_pumil
@@ -43,8 +43,11 @@ from pywsl.utils.comcalc import bin_clf_err
 import statsmodels.discrete.discrete_model as sm
 from sklearn.ensemble import RandomForestClassifier
 #NN
-
-
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torch.nn as nn
+import torch.optim as optim
+    
 #Learners
 def deconfounder_PPCA_LR(train,colnames,y01,name,k,b):
     '''
@@ -491,3 +494,122 @@ def meta_learner(data1, models):
     roc = {'metalearners': 'ensemble','precision':pr ,'recall':re,'auc':auc,'f1':f1,'f1_':f1_}
     roc_table = roc_table.append(roc,ignore_index=True)
     return roc_table
+
+
+def nn_classifier(y_train, y_test, X_train, X_test, EPOCHS, BATCH_SIZE, LEARNING_RATE):
+    
+    
+    #https://towardsdatascience.com/pytorch-tabular-binary-classification-a0368da5bb89
+
+    
+    class trainData(Dataset):
+        
+        def __init__(self, X_data, y_data):
+            self.X_data = X_data
+            self.y_data = y_data
+            
+        def __getitem__(self, index):
+            return self.X_data[index], self.y_data[index]
+            
+        def __len__ (self):
+            return len(self.X_data)
+    
+    
+    train_data = trainData(torch.FloatTensor(X_train.to_numpy()), 
+                           torch.FloatTensor(y_train.to_numpy()))
+    ## test data    
+    class testData(Dataset):
+        
+        def __init__(self, X_data):
+            self.X_data = X_data
+            
+        def __getitem__(self, index):
+            return self.X_data[index]
+            
+        def __len__ (self):
+            return len(self.X_data)
+        
+    
+    test_data = testData(torch.FloatTensor(X_test.to_numpy()))
+    test_data_full = testData(torch.FloatTensor(pd.concat([X_train, X_test],axis=0).to_numpy()))
+    #test_data = testData(torch.FloatTensor(X_test.to_numpy()))
+    
+    train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE)
+    test_loader = DataLoader(dataset=test_data, batch_size=1)
+    test_full = DataLoader(dataset = test_data_full,batch_size=1)
+    y_test_full = pd.concat([y_train,y_test],axis =0).to_numpy()
+    
+    
+    class binaryClassification(nn.Module):
+        def __init__(self):
+            super(binaryClassification, self).__init__()
+            # Number of input features is 12.
+            self.layer_1 = nn.Linear(8, 44) 
+            self.layer_2 = nn.Linear(44, 1)
+            #self.layer_out = nn.Linear(16, 1) 
+            
+            self.relu = nn.ReLU()
+            self.sigmoid = nn.Sigmoid()
+            self.dropout = nn.Dropout(p=0.2)
+            self.batchnorm1 = nn.BatchNorm1d(128)
+            
+        def forward(self, inputs):
+            x = self.relu(self.layer_1(inputs))
+            x = self.batchnorm1(x)
+            x = self.dropout(x)
+            x = self.sigmoid(self.layer_2(x))
+            #x = self.layer_out(x)
+            
+            return x
+        
+    model = binaryClassification()
+    #model.to(device)
+    #print(model)
+    criterion = nn.BCELoss()#BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    
+    model.train()
+    for e in range(1, EPOCHS+1):
+        epoch_loss = 0
+        for X_batch, y_batch in train_loader:
+            optimizer.zero_grad()
+            y_pred = model(X_batch)
+            loss = criterion(y_pred, y_batch.unsqueeze(1))
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+    
+    print(f'Epoch {e+0:03}: | Loss: {epoch_loss/len(train_loader):.5f}')
+    
+        
+    y_pred_list = []
+    model.eval()
+    with torch.no_grad():
+        for X_batch in test_loader:
+            y_test_pred = model(X_batch)
+            #y_test_pred = torch.sigmoid(y_test_pred)
+            #y_pred_tag = torch.round(y_test_pred)
+            y_pred_list.append(y_test_pred[0].detach().numpy())
+            
+    
+    y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+    y_pred_list = [1 if a>0.5 else 0 for a in y_pred_list]
+    f1_ = f1_score(y_test, y_pred_list)
+    print(confusion_matrix(y_test, y_pred_list))
+    
+    y_pred_list_full = []
+    model.eval()
+    with torch.no_grad():
+        for X_batch in test_full:
+            #X_batch = X_batch.to(device)
+            y_test_pred = model(X_batch)
+            #y_test_pred = torch.sigmoid(y_test_pred)
+            #y_pred_tag = torch.round(y_test_pred)
+            y_pred_list_full.append(y_test_pred[0].detach().numpy())
+    
+    y_pred_list_full = [a.squeeze().tolist() for a in y_pred_list_full]
+    y_pred_list_full = [1 if a>0.5 else 0 for a in y_pred_list_full]
+    f1 = f1_score(y_test_full, y_pred_list_full)
+    print(confusion_matrix(y_test_full, y_pred_list_full))
+    return f1_, f1
+
